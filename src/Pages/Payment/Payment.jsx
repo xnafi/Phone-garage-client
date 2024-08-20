b06af29968d87fc8837f09a0a1a3f17c0a646a8c
Forhad Khan <87197002+xnafi@users.noreply.github.com>
2024-08-20 17:00:53 +0600

    changes done

--- src/Pages/Payment/Payment.jsx
+++ src/Pages/Payment/Payment.jsx
@@ -12,7 +12,7 @@ const Payment = () => {
     const paymentDetails = useLoaderData()
 
     return (
-        <div className='my-10 px-4 lg:px-10 items-center'>
+        <div className='my-10 px-4 lg:px-10 items-center mt-[100px]'>
             <div>
                 <h2 className='text-2xl font-bold'>Payment for {paymentDetails?.productName} model {paymentDetails.productModel}</h2>
                 <h2 className='text-xl font-semibold'>Please pay {paymentDetails?.productPrice}</h2>
